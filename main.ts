218223b8b77dc0098bc6981a3025feb68d05b23d
sadahussain123567 <systemhang377@gmail.com>
2024-05-19 08:37:42 -0700

    Initial commit

---
+++ main.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+const ans:{
+    sent:string;
+
+}=await inquirer.prompt([
+    {
+        name:"sent",
+        type:"input",
+        message:chalk.greenBright.bold("enter your sentence to count the words")
+    }
+])
+const done=ans.sent.trim().split(" ")
+console.log(done)
+console.log(`your sentence word count is ${done.length}`)
